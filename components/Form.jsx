83b2d73e3b1244a7907006a4d35bb5d41c724d88
Beau Elliott <belliott1516@gmail.com>
2023-07-05 10:30:47 -0700

    getting create prompt page ready and updating form.jsx as a reusable component

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import Link from "next/link";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start">
+      <h1>{type} Post</h1>
+    </section>
+  );
 };
 
 export default Form;
